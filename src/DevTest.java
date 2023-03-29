be277f098ed6c2d0ae16e32cf5983e374faff9ee
keytonar <austinkeyton@gmail.com>
2023-03-29 09:32:52 -0400

    Ran getInt test and getDouble test

--- src/DevTest.java
+++ src/DevTest.java
@@ -4,11 +4,19 @@ public class DevTest
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        String trash = "";
         String firstName = "";
         String lastName = "";
         firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
         lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
         System.out.println("\nYour full name is: " + firstName + " " + lastName);
+
+        int favInt = 0;
+        double favDouble = 0;
+        favInt = SafeInput.getInt(in, "Enter your favorite integer: ");
+        favDouble = SafeInput.getDouble(in, "Enter your favorite double: ");
+
     }
+
 
 }
