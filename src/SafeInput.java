0d3252568ca598fa8a0e8ea1d1df2a99f7d551ce
keytonar <austinkeyton@gmail.com>
2023-03-28 20:57:11 -0400

    Added int safe input

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -19,6 +19,29 @@ public class SafeInput
         } while (retString.length() == 0);
 
         return retString;
-
     }
+    /**
+     * Prompts the user to input an integer value and validates the input.
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return an integer value entered by the user
+     */
+    public static int getInt(Scanner pipe, String prompt) {
+        int value = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print(prompt);
+            if (pipe.hasNextInt()) {
+                value = pipe.nextInt();
+                validInput = true;
+            } else {
+                String trash = pipe.next();
+                System.out.println("Invalid input. Please enter an integer value.");
+            }
+            pipe.nextLine(); // clear the newline character from the input stream
+        }
+        return value;
+    }
+
 }
