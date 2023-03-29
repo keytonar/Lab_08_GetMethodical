837a30c0b01a5bda7c302409329c01e13873b264
keytonar <austinkeyton@gmail.com>
2023-03-28 21:20:44 -0400

    Finished Safe input methods

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -59,6 +59,66 @@ public class SafeInput
         }
         return value;
     }
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int value = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextInt()) {
+                value = pipe.nextInt();
+                if (value >= low && value <= high) {
+                    validInput = true;
+                } else {
+                    System.out.println("Invalid input. Please enter an integer within the range [" + low + " - " + high + "].");
+                }
+            } else {
+                String trash = pipe.next();
+                System.out.println("Invalid input. Please enter an integer within the range [" + low + " - " + high + "].");
+            }
+            pipe.nextLine(); // clear the newline character from the input stream
+        }
+        return value;
+    }
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        double input = 0;
+        boolean validInput = false;
+        do {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextDouble()) {
+                input = pipe.nextDouble();
+                pipe.nextLine(); // clear the newline character
+                if (input >= low && input <= high) {
+                    validInput = true;
+                } else {
+                    System.out.println("Input must be within range [" + low + " - " + high + "]");
+                }
+            } else {
+                pipe.nextLine(); // clear the input
+                System.out.println("Invalid input, please enter a valid decimal number.");
+            }
+        } while (!validInput);
+        return input;
+    }
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String input;
+        do {
+            System.out.print(prompt + " [Y/N]: ");
+            input = pipe.nextLine().toUpperCase();
+        } while (!input.equals("Y") && !input.equals("N"));
+        return input.equals("Y");
+    }
+    public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+        String input = "";
+        do {
+            System.out.print(prompt + ": ");
+            while (!pipe.hasNextLine()) {
+                // Do nothing
+            }
+            input = pipe.nextLine().trim();
+            pipe.reset(); // Clear any delimiter from the pipe
+        } while (!input.matches(regEx));
+        return input;
+    }
 
 
 }
